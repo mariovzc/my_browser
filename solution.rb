720b69e31d57610d6e81743e4b7c72a928bf767f
¨Mario <mario.vizcainos@gmail.com>
2017-07-30 09:08:23 -0500

    update of the solution

--- solution.rb
+++ solution.rb
@@ -1,8 +1,5 @@
 require 'sinatra'
-require 'browser'
 
 get '/' do
-  browser = Browser.new request.user_agent, accept_language: request.env["HTTP_ACCEPT_LANGUAGE"]
-  browser.name                 # readable browser name
-
+  request.env["HTTP_ACCEPT_LANGUAGE"]
 end
